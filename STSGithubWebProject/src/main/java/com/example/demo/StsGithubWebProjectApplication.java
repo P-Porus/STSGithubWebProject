99428bf871866c05fa8340462bab63253b0d0876
Admin <puruaws@gmail.com>
2022-11-02 22:34:13 +0530

    First Commit

---
+++ STSGithubWebProject/src/main/java/com/example/demo/StsGithubWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsGithubWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsGithubWebProjectApplication.class, args);
+	}
+
+}
