02e5e547c8ad4564db6446bee776b7fe11b8e41b
Admin <puruaws@gmail.com>
2022-11-02 22:52:43 +0530

    Added Controller created endpoints /user/{user}

--- STSGithubWebProject/src/main/java/com/example/demo/StsGithubWebProjectApplication.java
+++ STSGithubWebProject/src/main/java/com/example/demo/StsGithubWebProjectApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class StsGithubWebProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StsGithubWebProjectApplication.class, args);
+	}
+	
+	
+	@GetMapping("/user/{user}")
+	public String getName(@PathVariable String user)
+	{
+		return "Welcome Mr "+user;
 	}
 
 }
